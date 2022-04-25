def9e9aac71609a58575eabe1e5c329f3da69b37
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-04-25 17:20:01 -0300

    feat: add work page

---
+++ src/pages/work.tsx
@@ -0,0 +1,14 @@
+import { Title } from '@mantine/core';
+import Head from 'next/head';
+
+export default function WorkPage() {
+  return (
+    <>
+      <Head>
+        <title>Wendell Kenneddy | Work</title>
+      </Head>
+
+      <Title>Work</Title>
+    </>
+  );
+}
