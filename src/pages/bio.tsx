173b870105f5f3b447c6f2e508eb9749faabcf19
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-04-25 17:19:23 -0300

    feat: add bio page

---
+++ src/pages/bio.tsx
@@ -0,0 +1,14 @@
+import { Title } from '@mantine/core';
+import Head from 'next/head';
+
+export default function BioPage() {
+  return (
+    <>
+      <Head>
+        <title>Wendell Kenneddy | Bio</title>
+      </Head>
+
+      <Title>Bio</Title>
+    </>
+  );
+}
