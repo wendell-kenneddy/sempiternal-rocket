9c2e1e683371275467f81ca4ecc7f3f792b4e9ef
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-04-25 17:19:43 -0300

    feat: add contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,14 @@
+import { Title } from '@mantine/core';
+import Head from 'next/head';
+
+export default function ContactPage() {
+  return (
+    <>
+      <Head>
+        <title>Wendell Kenneddy | Contact</title>
+      </Head>
+
+      <Title>Contact</Title>
+    </>
+  );
+}
