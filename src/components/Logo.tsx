92f35e5a490ee5bae8d782bc34cc798eb5e9d02b
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-04-26 16:52:54 -0300

    feat(logo_comp): add animation with framer-motion

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,31 +1,38 @@
-import { Box, Text } from '@mantine/core';
+import { Text } from '@mantine/core';
 import Link from 'next/link';
 import { FaRocket } from 'react-icons/fa';
 import { useMantineTheme } from '@mantine/core';
 import { useHover } from '@mantine/hooks';
+import { motion } from 'framer-motion';
 
 export function Logo() {
   const { colors } = useMantineTheme();
   const { hovered, ref } = useHover<HTMLAnchorElement>();
+  const {
+    spacing: { sm }
+  } = useMantineTheme();
 
   return (
     <Link href="/" passHref>
-      <Box
+      <motion.a
         ref={ref}
-        component="a"
-        sx={({ spacing }) => ({
+        style={{
           display: 'flex',
           alignItems: 'center',
-          gap: spacing.xs,
+          gap: sm,
           textDecoration: 'none'
-        })}
+        }}
+        initial={{ y: -20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: 20, opacity: 0 }}
+        transition={{ duration: 0.2 }}
       >
         <FaRocket color={hovered ? colors.teal[4] : '#fff'} fontSize={18} />
 
         <Text size="md" color="#fff" weight={700}>
           Wendell Kenneddy
         </Text>
-      </Box>
+      </motion.a>
     </Link>
   );
 }
