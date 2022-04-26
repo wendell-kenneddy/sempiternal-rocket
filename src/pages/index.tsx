48f3e12c90b4253d5efb9fee2d0a7b05afa17d2c
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-04-26 16:59:32 -0300

    feat(homepage): update content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
-import { Title } from '@mantine/core';
+import { Box, Title } from '@mantine/core';
 import Head from 'next/head';
+import { AnimatedLayout } from '../components/Layout/AnimatedLayout';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { RotatingSphere } from '../components/RotatingSphere';
 
 export default function Home() {
   return (
@@ -8,7 +12,28 @@ export default function Home() {
         <title>Wendell Kenneddy | Homepage</title>
       </Head>
 
-      <Title>Hello, world!</Title>
+      <AnimatedLayout>
+        <Box
+          sx={() => ({
+            width: 280,
+            height: 280,
+            margin: '0 auto',
+            '@media (min-width: 768px)': {
+              width: 540,
+              height: 540
+            }
+          })}
+        >
+          <Canvas>
+            <RotatingSphere />
+            <ambientLight intensity={0.1} />
+            <directionalLight color="red" position={[0, 0, 20]} />
+            <OrbitControls />
+          </Canvas>
+        </Box>
+
+        <Title>Hello, world!</Title>
+      </AnimatedLayout>
     </>
   );
 }
