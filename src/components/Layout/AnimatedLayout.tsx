0b8d8d67bddace2b3e8b179f5aa03b51e3d2d661
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-04-26 16:50:58 -0300

    feat(comps): add AnimatedLayout

---
+++ src/components/Layout/AnimatedLayout.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react';
+import { motion } from 'framer-motion';
+
+interface AnimatedLayoutProps {
+  children: ReactNode;
+}
+
+const variants = {
+  hidden: { opacity: 0, x: 0, y: 20 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: -0, y: 20 }
+};
+
+export function AnimatedLayout({ children }: AnimatedLayoutProps) {
+  return (
+    <motion.article
+      initial="hidden"
+      animate="enter"
+      exit="exit"
+      variants={variants}
+      transition={{ duration: 0.4 }}
+      style={{ position: 'relative' }}
+    >
+      {children}
+    </motion.article>
+  );
+}
