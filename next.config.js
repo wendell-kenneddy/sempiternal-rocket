2f906a566c199ca08f5568b97d3da7fc7c20c8ab
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-04-26 16:58:36 -0300

    build(next_config): add support to threejs add-ons

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+const withTM = require('next-transpile-modules')(['three']);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  reactStrictMode: true
+};
 
-module.exports = nextConfig
+module.exports = withTM(nextConfig);
