1fea522e3b48de3b31c8aa8b05feacb0d1f0a20c
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-04-28 16:35:37 -0300

    refactor(githublink_comp): turn into a plain anchor instead of next link

--- src/components/Navbar/GithubLink.tsx
+++ src/components/Navbar/GithubLink.tsx
@@ -1,5 +1,4 @@
 import { Box, Text } from '@mantine/core';
-import Link from 'next/link';
 import { FaGithub } from 'react-icons/fa';
 
 interface GithubLinkProps {
@@ -25,24 +24,24 @@ export function GithubLink({ asText }: GithubLinkProps) {
       <FaGithub color="#fff" fontSize={18} />
     </Text>
   ) : (
-    <Link href="https://github.com/wendell-kenneddy/portfolio" passHref>
-      <Box
-        component="a"
-        sx={({ spacing, colors, fn: { darken } }) => ({
-          display: 'flex',
-          alignItems: 'center',
-          gap: spacing.sm,
-          textDecoration: 'none',
-          color: darken('#fff', 0.4),
-          cursor: 'pointer',
-          ':hover': {
-            color: colors.teal[4]
-          }
-        })}
-      >
-        Source
-        <FaGithub color="#fff" fontSize={18} />
-      </Box>
-    </Link>
+    <Box
+      component="a"
+      href="https://github.com/wendell-kenneddy/portfolio"
+      target="_blank"
+      sx={({ spacing, colors, fn: { darken } }) => ({
+        display: 'flex',
+        alignItems: 'center',
+        gap: spacing.sm,
+        textDecoration: 'none',
+        color: darken('#fff', 0.4),
+        cursor: 'pointer',
+        ':hover': {
+          color: colors.teal[4]
+        }
+      })}
+    >
+      Source
+      <FaGithub color="#fff" fontSize={18} />
+    </Box>
   );
 }
