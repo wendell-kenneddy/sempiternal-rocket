001c83aafe8dc2e40b6c79c4e7c5f26361a2c97b
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-05-07 13:00:10 -0300

    feat(comps): add ProjectCard

---
+++ src/components/Projects/ProjectCard.tsx
@@ -0,0 +1,37 @@
+import { Card, Image, Text, Title } from '@mantine/core';
+import { ProjectData } from '.';
+
+interface ProjectCardProps {
+  project: ProjectData;
+}
+
+export function ProjectCard({ project }: ProjectCardProps) {
+  const { title, description, image, url } = project;
+
+  return (
+    <Card
+      component="a"
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      shadow="md"
+      sx={({ colors }) => ({
+        width: '100%',
+        border: '1px solid transparent',
+        ':hover': {
+          borderColor: colors.teal[4]
+        }
+      })}
+    >
+      <Card.Section>
+        <Image src={image} alt={`${title} project icon`} height={132} />
+      </Card.Section>
+
+      <Title mt="xs" order={3}>
+        {title}
+      </Title>
+
+      <Text size="xs">{description}</Text>
+    </Card>
+  );
+}
