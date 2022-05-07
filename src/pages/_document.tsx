698fe850cbccff0c26dd5db5716dd23f95908abf
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-05-07 12:58:04 -0300

    feat(next_document): add icons and manifest links

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,10 @@ export default class _Document extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
+          <link rel="icon" href="/icons/favicon.ico" sizes="any" />
+          <link rel="icon" href="/icons/icon.svg" type="image/svg+xml" />
+          <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+          <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
           <Main />
